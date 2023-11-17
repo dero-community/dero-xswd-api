ca27b538cd00b70fe1858995733f4c6c2b007201
Evan Graïne <evan.graine@gmail.com>
2023-11-17 14:41:11 +0100

    type fix revert

--- src/types/response.ts
+++ src/types/response.ts
@@ -91,7 +91,7 @@ type Status = { status: "OK" };
 
 type DEROGetInfoResult = {
   alt_blocks_count: Uint64;
-  difficulty: string;
+  difficulty: Uint64;
   grey_peerlist_size: number;
   height: Uint64;
   stableheight: Uint64;
